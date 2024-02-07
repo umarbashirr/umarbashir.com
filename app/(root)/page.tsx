7f38bcc29560b9812dcfc697486ff03dae7cfb54
gfxcode <umar.bashir@staybloom.com>
2024-02-07 17:07:03 +0530

    updated jsonld schema for homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -8,6 +8,7 @@ import {
 } from "../../components/HomeComponents";
 import Portfolio from "../../components/Portfolio/Portfolio";
 import SectionWithMargin from "../../components/SectionWithMargin";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Home | Umar Bashir Portfolio | Software Developer",
@@ -16,9 +17,39 @@ export const metadata: Metadata = {
   },
 };
 
+function addPersonJsonLd() {
+  return {
+    __html: `{
+      "@context": "https://schema.org",
+      "@type": "Person",
+      name: "Umar Bashir Rather",
+      url: "https://www.umarbashir.com",
+      // image: "https://www.umarbashir.com/images/your-image.jpg",
+      sameAs: [
+        "https://www.linkedin.com/in/umarbashirr/",
+        "https://github.com/umarbashirr",
+        "https://twitter.com/umarbashirr",
+      ],
+      jobTitle: "Software Developer",
+      worksFor: {
+        "@type": "Organization",
+        name: "Self-Employed",
+      },
+      email: "mailto:umarbashir601@gmail.com",
+    }`,
+  };
+}
+
 export default function Home() {
   return (
     <>
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={addPersonJsonLd()}
+          key="person-jsonld"
+        />
+      </Head>
       <HeroBanner />
       <WhyUsSection />
       <AboutSection />
